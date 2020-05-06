789e475415593b0eed0907d30ce946026a3a3302
teaset <teasetstudio@users.noreply.github.com>
2020-05-06 19:15:06 +0300

    l

--- Vince/js/vince.js
+++ Vince/js/vince.js
@@ -13,3 +13,11 @@ function buttonMenu (){
         navMenu.style.marginTop = "-30px";
         dis = 0;
 }}
+
+var oldWid = window.innerWidth;
+window.onresize = function(){
+  if (oldWid < 529 && oldWid < window.innerWidth && window.innerWidth > 529){
+    location.reload();
+    oldWid = window.innerWidth;
+  }else oldWid = window.innerWidth;
+}
