4b417fbbb1d33b7516e1cfcee6a59134c2e3d51d
teaset <teasetstudio@users.noreply.github.com>
2020-05-06 12:07:53 +0300

    as

--- Vince/js/vince.js
+++ Vince/js/vince.js
@@ -6,8 +6,10 @@ var height = navMenu.scrollHeight;
 function buttonMenu (but){
   if (dis == 0) {
     navMenu.style.height = height+"px";
+    navMenu.style.marginTop = "5px";
     dis = 1;
     } else {
         navMenu.style.height = 0;
+        navMenu.style.marginTop = "-30px";
         dis = 0;
 }}
