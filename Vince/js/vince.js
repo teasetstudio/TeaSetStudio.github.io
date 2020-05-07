c85fcf7ef28f7d783b72fd8d10d1ebe2597885c0
teaset <teasetstudio@users.noreply.github.com>
2020-05-07 19:36:08 +0300

    ычы

--- Vince/js/vince.js
+++ Vince/js/vince.js
@@ -1,23 +1,41 @@
-// MOBILE DROPDOWN MENU;
+// MOBILE DROPDOWN MENU ANIMATION =================================
 const navMenu = document.querySelector('.nav-main');
 var dis = 0;
-
+var relod = 0;
 function buttonMenu (){
   var height = navMenu.scrollHeight;
   if (dis == 0) {
     navMenu.style.height = height+"px";
     navMenu.style.marginTop = "5px";
     dis = 1;
+    relod = 7;
     } else {
-        navMenu.style.height = 0;
-        navMenu.style.marginTop = "-30px";
-        dis = 0;
+      navMenu.style.height = 0;
+      navMenu.style.marginTop = "-30px";
+      dis = 0;
 }}
 
-var oldWid = window.innerWidth;
+// refresh break point 529px if mobile menu is used =============
 window.onresize = function(){
-  if (oldWid < 529 && oldWid < window.innerWidth && window.innerWidth > 529){
+  if (window.innerWidth > 529 && relod > 0){
     location.reload();
-    oldWid = window.innerWidth;
-  }else oldWid = window.innerWidth;
-}
+    relod = 0;
+}}
+
+// set zoom event (external code)==================================
+window.onzoom = function(e) {
+	// zoom event
+};
+// detect resize
+(function() {
+	var oldresize = window.onresize;
+	window.onresize = function(e) {
+    var event = window.event || e;
+    if(typeof(oldresize) === 'function' && !oldresize.call(window, event)) {
+      return false;
+    }
+    if(typeof(window.onzoom) === 'function') {
+      return window.onzoom.call(window, event);
+    }
+  }
+})();
