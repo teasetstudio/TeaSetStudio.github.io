a4c031b7759f0c756411bfced9f3779f7ac16312
teaset <teasetstudio@users.noreply.github.com>
2020-05-07 20:25:42 +0300

    п

---
+++ fbp/js/main.js
@@ -0,0 +1,45 @@
+const toFormBtn = document.querySelector('.btn');
+const cube1 = document.querySelector('.cube');
+var angleRot = 18;
+
+//ROTATE CUBE1 TO FORM ==============================================
+function rotateToForm (){
+    do{
+        angleRot -=90;}
+    while (angleRot%360 != -72);
+    cube1.style.transform = "translateZ(-170px) rotateY("+ angleRot + "deg)";
+}
+
+// ROTATE CUBE1 -90 DEGREES ==========================================
+function rotate90 (){
+    angleRot -= 90;
+    cube1.style.transform = "translateZ(-170px) rotateY("+ angleRot + "deg)";}
+
+// CHOOSE ANIMATIONS FOR CUBE2 =======================================
+// var radioAnim2 = document.getElementsByName('cubeAnimation');
+// const cube2 = document.querySelector ('.cube2');
+
+// for (var i = 0; i<radioAnim2.length; i++){
+//    radioAnim2[i].onchange = changeAnim;}
+
+// function changeAnim(){
+//     if (this.value == 'one'){
+//         cube2.style.animationPlayState = "running";
+//         cube2.style.animationName = "halfCircleAnim";
+//         cube2.style.animationDuration = "5s";
+//         cube2.style.animationDirection = "alternate";
+//         cube2.style.animationTimingFunction = "ease-in-out";
+//         cube2.style.animationIterationCount = "infinite";
+//     }
+//     else if (this.value == 'two'){
+//         cube2.style.animationPlayState = "running";
+//         cube2.style.animationName = "circleAnim";
+//         cube2.style.animationDuration = "20s";
+//         cube2.style.animationDirection = "normal";
+//         cube2.style.animationTimingFunction = "linear";
+//         cube2.style.animationIterationCount = "infinite";
+//     } else {
+//         cube2.style.animationDuration = "20s";
+//         cube2.style.animationName = "aninOff";
+//         cube2.style.animationIterationCount = "1";
+// }}
