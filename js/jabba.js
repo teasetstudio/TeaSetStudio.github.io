f6de4348906a2c9ea2b34dac60a52b7b657e6bbe
teaset <teasetstudio@users.noreply.github.com>
2020-05-14 12:03:05 +0300

    1

--- js/jabba.js
+++ js/jabba.js
@@ -2,7 +2,6 @@ const theToggle = document.getElementById('toggle');
 let toggleOnOff = 0;
 
 window.onclick = function (e){
-    alert(e.target);
     if (e.target != theToggle || toggleOnOff == 1){
         theToggle.className = '';
         toggleOnOff = 0;
