a55270ff6e005bfa59f346a254da36efa4fa433a
teaset <teasetstudio@users.noreply.github.com>
2020-06-02 13:11:08 +0300

    asd

--- manterus/js/formRequest.js
+++ manterus/js/formRequest.js
@@ -10,7 +10,7 @@ window.onload = function () {
         const fmessage = form.elements['fmessage'].value;
         submitButton.disabled = true;
 
-        fetch('localhost:3000/test',{
+        fetch('http://localhost:3000/test',{
             method: 'POST',
             headers: {
                 'Accept': 'application/json',
