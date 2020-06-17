9e183de6c747291100d243f0e4faec82bc1ee762
teaset <teasetstudio@users.noreply.github.com>
2020-06-17 16:06:57 +0300

    sd

---
+++ memoryJS/js/memory.js
@@ -0,0 +1,113 @@
+let memoInput = document.getElementById('memo-body'),
+    NUM_IMAGES_IN_FOLDER = 50,
+    rows = 5,
+    collums = 8,
+    numItems = rows * collums, // must be even number!!!
+    cardsList = [],
+    imageOrder = {
+        images: [],
+        att: []
+    };
+
+let start={
+        addElement:function(image, idName, att){
+            let newDiv = document.createElement("div");
+            newDiv.innerHTML = "<div id=\"" + idName + "\" target=\"" + att + "\" isopen=\"no\" class=\"memo\"><div class=\"memo-face\"><img src=\"./img/"+ image + "\"></div>  <div class=\"memo-back\"></div>  </div>";
+            memoInput.appendChild(newDiv);
+        },
+        createTableItem: function(array, att){
+            for (let i=0; i<array.length; i++)
+                this.addElement (array[i], "A0"+ i, att[i]);
+        },
+        createArray: function (){
+            // image names array (data base) and image order array
+            for (let i=1; i<numItems+1; i++){
+                cardsList.push (i + ".png");
+            };
+            //images sequence array (order of cards)
+
+            for (let i=0; i<(numItems/2); i++){
+                let randNum1 = Math.floor(Math.random()*numItems),
+                    randNum2 = Math.floor(Math.random()*numItems);
+                //checker
+                while (imageOrder.images[randNum1]){
+                    randNum1 = Math.floor(Math.random()*numItems);
+                }
+                while (randNum1===randNum2 || imageOrder.images[randNum2]){
+                    randNum2 = Math.floor(Math.random()*numItems);
+                }
+                //create array item
+                imageOrder.images[randNum1] = cardsList[i];
+                imageOrder.images[randNum2] = cardsList[i];
+                imageOrder.att[randNum1] = "A"+ i;
+                imageOrder.att[randNum2] = "A"+ i;
+            };
+        }
+    };
+
+start.createArray();
+start.createTableItem(imageOrder.images, imageOrder.att);
+
+// GAME process
+let nextClickIndex = 1, //1 - first memo, 2 - second
+    checkOnThirdClick = 1, // check situation whe
+    memoOne,
+    isOpen,
+    attOne,
+    attTwo,
+    score = 1,
+    memoTwo;
+
+let gameActions = {
+    definedMemo: function (currentMemo, e){
+        currentMemo = e.target.closest(".memo");
+        this.openMemo(currentMemo);
+        return currentMemo;
+    },
+    isSimillar: function (){
+        if (attOne === attTwo){
+            memoOne="";
+            memoTwo="";
+            score ++;
+        } else {
+            this.closeMemo (memoOne);
+            this.closeMemo (memoTwo);
+            memoOne = "";
+            memoTwo = "";
+        }
+    },
+    winGame: function (){
+        alert ("Congratulation! You won the game!");
+        location.reload();
+    },
+    openMemo: function (memo){
+        memo.style.transform = "rotateY(0deg)";
+        memo.setAttribute ("isopen", "da");
+    },
+    closeMemo: function (memo){
+        memo.style.transform = "rotateY(180deg)";
+        memo.setAttribute ("isopen", "no");
+    }
+};
+window.onclick = function (e){
+    currentMemo = e.target.closest(".memo");
+    isOpen = currentMemo.getAttribute("isopen");
+    if (checkOnThirdClick === 3){
+        gameActions.isSimillar();
+        checkOnThirdClick = 1;
+    }
+    if (nextClickIndex === 1 && isOpen != "da"){
+        memoOne = gameActions.definedMemo (memoOne, e);
+        attOne = memoOne.getAttribute("target");
+        nextClickIndex = 2;
+        checkOnThirdClick++;
+    } else if (nextClickIndex === 2 && isOpen != "da"){
+        memoTwo = gameActions.definedMemo (memoTwo, e);
+        attTwo = memoTwo.getAttribute("target");
+        nextClickIndex = 1;
+        checkOnThirdClick++;
+        if (score == numItems/2){
+            memoTwo.addEventListener ('transitionend', function(){
+                gameActions.winGame()})}
+    }
+};
