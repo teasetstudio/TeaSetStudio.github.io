406d96832ab1c5e1687eafd04a1f0d91bf0954d5
teaset <teasetstudio@users.noreply.github.com>
2020-06-27 21:08:14 +0300

    cv

--- memoryJS/js/memory.js
+++ memoryJS/js/memory.js
@@ -1,10 +1,13 @@
 let memoInput = document.getElementById('memo-body'),
     clickScore = document.querySelector('#clicks span'),
-    IMG_NUM_IN_FOLDER = 120,
-    rows = 5,
+    IMG_NUM_IN_FOLDER = 130,
+    rows = 5, // rows or collums must be even number!!!
     collums = 8,
-    numItems = rows * collums, // must be even number!!!
+    numItems = rows * collums,
     cardsList = [],
+    players = 1,
+    redScore = 0,
+    blueScore = 0,
     imageOrder = {
         images: [],
         att: []
@@ -17,16 +20,19 @@ let start={
             memoInput.appendChild(newDiv);
         },
         createTableItem: function(array, att){
+            memoInput.innerHTML = "";
             for (let i=0; i<array.length; i++)
                 this.addElement (array[i], "A0"+ i, att[i]);
         },
         createArray: function (){
-
+            cardsList = [];
+            imageOrder.images = [];
+            imageOrder.att = [];
+            numItems = rows * collums;
             // image names array (data base) and image order array
             for (let i=1; i<numItems+1; i++){
                 let rand = (Math.ceil (Math.random()*IMG_NUM_IN_FOLDER)) + ".png",
                     noRepeat = 0;
-
                 while (noRepeat === 0){
                     noRepeat = 1;
                     for (let i=0; i<cardsList.length; i++){
@@ -38,8 +44,7 @@ let start={
                     };
                 };
                 cardsList.push (rand);
-            };
-
+            }
             //images sequence array (order of cards)
             for (let i=0; i<(numItems/2); i++){
                 let randNum1 = Math.floor(Math.random()*numItems),
@@ -93,8 +98,18 @@ let gameActions = {
         }
     },
     winGame: function (){
-        alert ("Поздравляю! Ты выйграл!" + "\nВы справились за " + clicks + " кликов.");
-        location.reload();
+        if (players == 2){
+            if (redScore > blueScore){
+                alert ('Выйграл КРАСНЫЙ игрок!! Собрав ' + redScore + ' мемок.');
+            } else if(redScore < blueScore){
+                alert ('Выйграл СИНИЙ игрок!! Собрав ' + blueScore + ' мемок.');
+            }else alert ('Ничья...');
+            alert ("Вместе вы справились за " + clicks + " кликов.");
+            playerTwo();
+        }else {
+            alert ("Поздравляю! Ты выйграл!" + "\nТы справился за " + clicks + " кликов.");
+            playerOne();
+        };
     },
     openMemo: function (memo){
         memo.style.transform = "rotateY(0deg)";
@@ -122,10 +137,157 @@ window.onclick = function (e){
     } else if (nextClickIndex === 2 && isOpen != "da"){
         memoTwo = gameActions.definedMemo (memoTwo, e);
         attTwo = memoTwo.getAttribute("target");
+        if (players==2){
+            playersScore.isSimillar();
+        };
         nextClickIndex = 1;
         checkOnThirdClick++;
         if (score == numItems/2){
             memoTwo.addEventListener ('transitionend', function(){
                 gameActions.winGame()})}
     }
+};
+
+let menuDiv = document.getElementById('menu'),
+    recordsLvl1 = document.getElementById('level-1'),
+    recordsLvl2 = document.getElementById('level-2'),
+    closeMenuFunction = function (){
+        menuDiv.style.transform = "translateX(-400px)";    
+        menuDiv.removeEventListener('onmouseleave', closeMmenu);
+    };
+
+/* open/close menu */
+function openMenu (){
+    menuDiv.style.transform = "translateX(0px)"; 
+};
+menuDiv.onmouseleave = function(){
+    closeMenuFunction();
+};
+menuDiv.onclick = function(){
+    closeMenuFunction();
+};
+/* MENU LEVELS */
+function normStart(){
+    clicks = 0;
+    nextClickIndex = 1;
+    checkOnThirdClick = 1;
+    score = 1;
+    rows = 5;
+    collums = 8;
+    clickScore.innerHTML = clicks;
+    memoInput.style.setProperty('max-width', '1400px');
+    start.createArray();
+    start.createTableItem(imageOrder.images, imageOrder.att);
+    recordsLvl1.style.display = 'block';
+    recordsLvl2.style.display = 'none';
+};
+function hardStart(){
+    clicks = 0;
+    nextClickIndex = 1;
+    checkOnThirdClick = 1;
+    score = 1;
+    rows = 6;
+    collums = 10;
+    clickScore.innerHTML = clicks;
+    memoInput.style.setProperty('max-width', '1700px');
+    start.createArray();
+    start.createTableItem(imageOrder.images, imageOrder.att);
+    recordsLvl1.style.display = 'none';
+    recordsLvl2.style.display = 'block';
+// smooth scroll
+    let timerA = setInterval(function() {
+        if (window.pageYOffset > 70) clearInterval(timerA);
+        window.scrollBy (0, 5)
+    }, 30)
+};
+// // prevent image select\drag
+// window.onload = function() {
+//     document.body.onselectstart = function() {
+//         return false;
+//     }
+//     document.body.dragstart = function() {
+//         return false;
+//     }
+// }
+// player counter
+let records = document.getElementById('records'),
+    divTwoPlayers = document.getElementById('twoPlayers'),
+    slider = document.getElementById('playerSlider'),
+    redBtn = document.getElementById('redPlayer'),
+    blueBtn = document.getElementById('bluePlayer'),
+    redPlayer = document.querySelector('#redPlayer span'),
+    bluePlayer = document.querySelector('#bluePlayer span'),
+    currentPlayer; //1 -red, 2-blue
+
+let playersScore = {
+    showActive: function (){
+        if (currentPlayer == 1){
+            slider.style.transform = "translateX(-450px)";
+            slider.style.backgroundColor = "rgb(255, 226, 208)";
+            redBtn.style.boxShadow = "0 4px 8px 0";
+            redBtn.style.transform = "scale(1.1)";
+            redBtn.style.background = "rgb(218, 139, 91)";
+            blueBtn.style.boxShadow = "0 0 0 0";
+            blueBtn.style.transform = "scale(1)";
+            blueBtn.style.background = "rgb(136, 171, 187)";
+        } else {
+            slider.style.transform = "translateX(330px)";
+            slider.style.backgroundColor = "rgb(202, 244, 255)";
+            redBtn.style.boxShadow = "0 0 0 0";
+            blueBtn.style.boxShadow = "0 4px 8px 0";
+            redBtn.style.transform = "scale(1)";
+            blueBtn.style.transform = "scale(1.1)";
+            redBtn.style.background = "rgb(207, 167, 143)";
+            blueBtn.style.background = "rgb(72, 161, 202)";
+        };
+    },
+    isSimillar: function (){
+        if (attOne === attTwo){
+            this.openMemoPair();
+        } else {
+            if (currentPlayer == 1) currentPlayer = 2;
+            else currentPlayer = 1;
+            this.showActive();
+        }
+    },
+    openMemoPair: function(){
+        let memoOneBG = memoOne.querySelector('.memo-face'),
+            memoTwoBG = memoTwo.querySelector('.memo-face');
+        if (currentPlayer == 1){
+            redScore++;
+            redPlayer.innerHTML = redScore;
+            memoOneBG.style.background = 'rgb(218, 139, 91)';
+            memoTwoBG.style.background = 'rgb(218, 139, 91)';
+        }else{
+            blueScore++;
+            bluePlayer.innerHTML = blueScore;
+            memoOneBG.style.background = 'rgb(72, 161, 202)';
+            memoTwoBG.style.background = 'rgb(72, 161, 202)';
+        };
+    }
+}
+function playerOne (){
+    players = 1;
+    divTwoPlayers.style.display = 'none';
+    if (rows == 5){
+        normStart();
+    }else{
+        hardStart();
+    };
+};
+function playerTwo (){
+    redScore = 0;
+    blueScore = 0;
+    redPlayer.innerHTML = redScore;
+    bluePlayer.innerHTML = blueScore;
+    players = 2;
+    currentPlayer = Math.ceil (Math.random()*2);
+    divTwoPlayers.style.display = 'flex';
+    playersScore.showActive();
+    if (rows == 5){
+        normStart();
+    }else{
+        hardStart();
+    };
+    
 };
