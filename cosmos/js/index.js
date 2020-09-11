617955646c1a91a2e5909cdb83984cb4f6b25bd3
teaset <teasetstudio@users.noreply.github.com>
2020-09-11 21:49:47 +0300

    asd

--- cosmos/js/index.js
+++ cosmos/js/index.js
@@ -1,3 +1,4 @@
+//Under grid glow ===============================================
 let light = document.querySelector('.aboutme__light'),
     x = 0,
     y = 0;
@@ -18,3 +19,44 @@ function showGrid(headBlock){
         light.style.opacity = 0;
     }
 };
+// smooth scroll ==============================================
+function scrollToDiv (duration){
+    function getPosition (){
+        let div = document.querySelector(clickTargetID);
+        let i = div.offsetTop + div.offsetParent.offsetTop;
+        return i;
+    }
+    const clickTargetID = event.target.getAttribute('href');
+    const targetPosition = getPosition();
+    let startPosition = window.pageYOffset;
+    let difference = targetPosition - startPosition;
+    let start = null;
+a
+    if (Math.abs(difference) < 500) duration /= 2;
+    window.requestAnimationFrame(step);
+    function step(timestamp) {
+        if (!start) start = timestamp;
+        let progress = timestamp - start;
+        window.scrollTo (0, easeInOutSine(progress, startPosition, difference, duration));
+        if (progress < duration) window.requestAnimationFrame(step);}
+}
+//easing function
+function easeInOutSine(t, b, c, d) {
+	return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+}
+//To the Top button appearance
+let toTop = document.getElementById('toTop'),
+    about = document.getElementById('about');
+
+window.addEventListener('scroll', () => {
+    if(window.pageYOffset > about.offsetTop-200) {
+        toTop.style.display = 'block';
+    } else toTop.style.display = 'none';
+})
+//crutch
+toTop.onclick = ()=>{
+    toTop.style.opacity = '0';
+    setTimeout(function(){
+        toTop.style.opacity = '1';
+    },800);
+}
