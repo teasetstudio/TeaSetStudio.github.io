f8897880a69dd1d420ef7cfb27fcd7ea3e5c0d39
teaset <teasetstudio@users.noreply.github.com>
2020-09-11 22:18:33 +0300

    sd

--- cosmos/js/index.js
+++ cosmos/js/index.js
@@ -24,19 +24,19 @@ function scrollToDiv (duration){
     function getPosition (){
         let div = document.querySelector(clickTargetID);
         let i = div.offsetTop + div.offsetParent.offsetTop;
-        return i;
-    }
+        return i;}
     const clickTargetID = event.target.getAttribute('href');
     const targetPosition = getPosition();
     let startPosition = window.pageYOffset;
     let difference = targetPosition - startPosition;
     let start = null;
-a
     if (Math.abs(difference) < 500) duration /= 2;
+
     window.requestAnimationFrame(step);
     function step(timestamp) {
         if (!start) start = timestamp;
         let progress = timestamp - start;
+        //window.scrollTo(0, difference*(progress/duration) + startPosition)
         window.scrollTo (0, easeInOutSine(progress, startPosition, difference, duration));
         if (progress < duration) window.requestAnimationFrame(step);}
 }
