e0c33dee71c39ba94527582b47431ed5277b2f79
teaset <teasetstudio@users.noreply.github.com>
2020-09-14 16:59:40 +0300

    server

---
+++ cosmos/js/formRequest.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+    const form = document.querySelector("form");
+    const submitButton = document.querySelector("button[type=submit]");
+
+    function myFunction(event) {
+        event.preventDefault();
+        const name = form.elements['name'].value;
+        const email = form.elements['email'].value;
+        const fmessage = form.elements['fmessage'].value;
+        submitButton.disabled = true;
+
+        fetch('https://manterus.herokuapp.com/test',{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                fmessage,
+            }),
+        })
+                .then(function(response) {
+                    response.json().then((result) => {
+                        console.log(result);
+                        form.innerHTML = 'Ваш запрос успешно отправлен';
+                    });
+                })
+                .catch(function(myJson) {
+                    submitButton.disabled = false;
+                    console.log(myJson);
+                });
+    };
+    form.addEventListener('submit', myFunction);
+};
